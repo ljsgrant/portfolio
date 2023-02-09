c248edeb47ce5b1853e4efc37b4b4f785946820f
Louis_Grant <ljsgrant@gmail.com>
2023-02-09 11:19:58 +0000

    display will appear/disappear via script

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
+import { useLayoutEffect, useRef } from 'react';
 import './styles/styles.scss';
 
 export default function App() {
+  const displayMainRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const displayMain = displayMainRef.current;
+    const onScroll = () => {
+      // console.log(window.scrollY);
+      if (window.scrollY > 200) {
+        displayMainRef.current.style.display = 'block';
+        console.log(displayMainRef.current.style.display);
+      } else {
+        displayMainRef.current.style.display = 'none';
+        console.log(displayMainRef.current.style.display);
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
   return (
     <div className='App'>
-      <div className="scroll"></div>
-      <div className="project-display-main"></div>
-      <div className="monitor-left">
-        <div className="monitor-left_circle-bracket"></div>
+      <div className='scroll'></div>
+      <div ref={displayMainRef} className='project-display-main'></div>
+      <div className='monitor-left'>
+        <div className='monitor-left_circle-bracket'></div>
       </div>
-      <div className="monitor-right"></div>
+      <div className='monitor-right'></div>
+      {/* <div className='first'>first</div>
+      <div className='second'>
+        <div ref={div} className='inside'>
+          box in second
+        </div>
+      </div> */}
     </div>
   );
 }
