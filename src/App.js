1b316c9eb0746c1d8cec503b04f40063b0600400
Louis_Grant <ljsgrant@gmail.com>
2023-02-09 11:00:33 +0000

    working on basic layout: monitors and display

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './styles/styles.scss';
+
+export default function App() {
+
+  return (
+    <div className='App'>
+      <div className="scroll"></div>
+      <div className="project-display-main"></div>
+      <div className="monitor-left">
+        <div className="monitor-left_circle-bracket"></div>
+      </div>
+      <div className="monitor-right"></div>
+    </div>
+  );
+}
